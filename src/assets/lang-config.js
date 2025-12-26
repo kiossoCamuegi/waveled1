94a7ab070f92e0bb1a558fb3f259eaf62344f73e
Kiosso camuegi <72392628+kiossoCamuegi@users.noreply.github.com>
2025-12-26 11:42:20 +0000

    Add files via upload

---
+++ src/assets/lang-config.js
@@ -0,0 +1,9 @@
+window.__GOOGLE_TRANSLATION_CONFIG__ = {
+  languages: [
+    { title: "Português", name: "pt" },
+    { title: "Ingles", name: "en" },
+    { title: "Espanhol", name: "es" },
+    { title: "Frances", name: "fr" },
+  ],
+  defaultLanguage: "pt",
+};
