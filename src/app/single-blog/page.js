6be11c7583ca75debca5f06c99559f5328054fcb
Kiosso camuegi <72392628+kiossoCamuegi@users.noreply.github.com>
2026-01-05 01:53:23 +0000

    Update page.js

--- src/app/single-blog/page.js
+++ src/app/single-blog/page.js
@@ -2,18 +2,17 @@
 
 import FooterOneSection from "~/components/Section/Common/FooterOne/FooterOneSection";
 import HeaderTwo from "~/components/Section/Common/Header/HeaderTwo";
-import PageHeader from "~/components/Section/Common/PageHeader";
-import SingleBlogSection from "~/components/Section/SingleBlog/SingleBlogSection";
+import PageHeader from "~/components/Section/Common/PageHeader"; 
 
 const SingleBlogPage = () => {
     return (
         <div>
             <HeaderTwo className="tekup-header-top bg-light1 "/>
-            <PageHeader title="Technology support allows erie non-profit to serve"/>
-            <SingleBlogSection/>
+            <PageHeader title="Technology support allows erie non-profit to serve"/> 
             <FooterOneSection className="tekup-footer-section dark-bg"/>
         </div>
     );
 };
+
 
 export default SingleBlogPage;
